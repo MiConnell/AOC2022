6fcb0dc97875aab2f46a9fba02c2d9cdfcafd853
michael <connellmp@gmail.com>
2022-12-02 22:04:48 -0800

    solve part 2

--- day02/part2.py
+++ day02/part2.py
@@ -9,11 +9,24 @@ def file_reader(file: str) -> str:
 
 
 def solver(s: str) -> int:
-    out = sorted(
-        sum(int(line) for line in part.splitlines()) for part in s.split("\n\n")
-    )
-    return sum(out[:-3])
-
+    _translation = {'A': 'R', 'X': 'R', 'B': 'P', 'Y': 'P', 'C': 'S', 'Z': 'S'}
+    weights = {'R': 1, 'P': 2, 'S': 3}
+    values = {'X': 0, 'Y': 3, 'Z': 6}
+    winning_losing_scores = {'R': 'S', 'P': 'R', 'S': 'P'}
+    losing_scores = {v: k for k, v in winning_losing_scores.items()}
+    total_score = 0
+    for line in s.splitlines():
+        opp_orig, my_orig = line.split(' ')
+        opp, my = _translation[opp_orig], _translation[my_orig]
+        total_score += values[my_orig]
+        if values[my_orig] == 0:
+            total_score += weights[winning_losing_scores[opp]]
+        elif values[my_orig] == 3:
+            total_score += weights[opp]
+        elif values[my_orig] == 6:
+            total_score += weights[losing_scores[opp]]
+    print(total_score)
+    return 0
 
 if __name__ == "__main__":
-    print(solver(file_reader(file)))
+    raise SystemExit(solver(file_reader(file)))
